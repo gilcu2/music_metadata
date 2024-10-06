73d3eefc76cb826b26f97d415e4a609e0f0a7159
Reynaldo Gil <gilcu2@gmail.com>
2024-10-06 07:13:57 +0200

    working on async test

--- src/test/scala/ServerSpec1.scala
+++ src/test/scala/ServerSpec1.scala
@@ -1,14 +1,9 @@
 import Configs.Config
 import Models._
 import cats.effect.testing.scalatest.AsyncIOSpec
-import cats.effect.unsafe.IORuntime.global
 import cats.effect.{ExitCode, IO}
 import io.circe.Json
 import io.circe.generic.auto._
-import io.circe.literal._
-import io.circe.Decoder
-import io.circe.generic.auto._
-import io.circe.parser._
 import io.circe.syntax._
 import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.circe._
@@ -19,31 +14,28 @@ import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, GivenWhenThen, funspec}
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.ExecutionContext
-
-class ServerSpec
+class ServerSpec1
   extends funspec.AsyncFunSpec
-  with AsyncIOSpec
-  with GivenWhenThen
-  with Matchers
-  with BeforeAndAfterAll
-  with Eventually {
+    with AsyncIOSpec
+    with GivenWhenThen
+    with Matchers
+    with BeforeAndAfterAll
+    with Eventually {
 
   private val configFile = "test.conf"
   implicit override val patienceConfig: PatienceConfig =
     PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(100, Millis)))
 
-  private lazy val config  = Config.load(configFile).use(config => IO.pure(config)).unsafeRunSync()
-  private val logger       = LoggerFactory.getLogger(classOf[ServerSpec])
+  private lazy val config = Config.load(configFile).use(config => IO.pure(config)).unsafeRunSync()
+  private val logger = LoggerFactory.getLogger(classOf[ServerSpec1])
+  private lazy val client = BlazeClientBuilder[IO].resource
   private lazy val rootUrl = s"http://${config.server.host}:${config.server.port}/api"
 
-  private val clientResource  = BlazeClientBuilder[IO].resource
 
   override def beforeAll(): Unit = {
-
     Server.create(configFile).unsafeRunAsync(resultHandler)
     eventually {
-      clientResource
+      client
         .use(_.statusFromUri(Uri.unsafeFromString(s"$rootUrl/hello/test")))
         .unsafeRunSync() shouldBe Status.Ok
     }
@@ -52,24 +44,25 @@ class ServerSpec
 
   describe("Repository server") {
 
-    it("create and retrieve an Artist1") {
+    it("create and retrieve an Artist") {
       Given("Artist")
       val artist = Artist(name = "Juan Gabriel")
+
+      When("Post and get the Artist")
       val requestPost =
         Request[IO](method = Method.POST, uri = Uri.unsafeFromString(s"$rootUrl/artist"))
           .withEntity(artist.asJson)
-
-      When("Post and get the Artist")
-      val r = clientResource.use ( client =>
-        for {
-        response <- client.use(_.expect[Json](requestPost))
-      } yield response
-    )
+      val jsonPost = client.use(_.expect[Json](requestPost)).unsafeRunSync()
+      val resultPost = jsonPost.as[Artist].toOption.get
+      val id = resultPost.id.get
+      val requestGet =
+        Request[IO](method = Method.GET, uri = Uri.unsafeFromString(s"$rootUrl/artist/$id"))
+      val jsonGet = client.use(_.expect[Json](requestGet)).unsafeRunSync()
+      val resultGet = jsonGet.as[Artist].toOption.get
 
       Then("result is the expected")
-      assert(r.status mustBe Status.Ok)
+      assert(resultGet.name == artist.name)
     }
-
 
   }
 
